016282a33a47f64e5dd6db0c9897e022ce4d163f
Jewar-PL <polskiendoangel2@gmail.com>
2022-12-23 19:43:38 +0100

    poprawka

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Odpowiedź bota
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-ale-zepsuty-2mop.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
